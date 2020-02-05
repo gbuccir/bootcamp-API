b9bb0d7261d6bae85857bdeaed59cbc3f0c879c7
gbuccir <gbucci@uol.com.br>
2020-02-04 22:54:39 -0300

    autenticacao jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '566700d79623947abe9738194c201f98',
+    expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 // import User from './app/models/user';
 import UserController from './app/controller/UserController';
+import SessionController from './app/controller/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 // routes.get('/', async (req, res) => {
 //     const user = await User.create({
