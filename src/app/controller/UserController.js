9b22bcf79431ecc397ad96de5a9ebe2f5f3b81ea
gbuccir <gbucci@uol.com.br>
2020-02-16 21:45:22 -0300

    autenditcacao jwt e update usuario com middleware

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -16,6 +16,42 @@ class UserController {
 
         return res.json({ id, name, email, provider });
     }
+
+    async pegar(req, res) {
+        const user = await User.findAll({});
+
+        return res.json(user);
+    }
+
+    async update(req, res) {
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (email && email !== user.email) {
+            const userExists = await User.findOne({
+                where: {
+                    email,
+                },
+            });
+
+            if (userExists) {
+                return res.status(400).json({ error: 'user already exists.' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({ erro: 'password does not match' });
+        }
+        const { id, name, provider } = await user.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+            provider,
+        });
+    }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 // import User from './app/models/user';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.get('/users', UserController.pegar);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 // routes.get('/', async (req, res) => {
 //     const user = await User.create({
